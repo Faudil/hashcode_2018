2831f13f026e3ac3742338c6cdd605e599bb3dc8
Tanguy GEROME <tanguy.gerome@epitech.eu>
2018-03-01 19:24:52 +0100

    added Vehicle total_ride_legth

--- hashcode.py
+++ hashcode.py
@@ -1,10 +1,22 @@
 class Ride:
     def __init__(self, begin, end, start, finish):
-        _begin = begin
-        _end = end
-        _start = start
-        _finish = finish
+        self._begin = begin
+        self._end = end
+        self._start = start
+        self._finish = finish
 
 class Vehicle:
     def __init__(self):
-        _avail = True
+        self._avail = True
+        self._rides = []
+
+    def total_ride_length(self, ride):
+        x_b, y_b = ride._begin
+        x_e, y_e = ride._end
+        x_p, y_p = _pause
+        sum = abs(x_p - x_b)
+        sum += abs(y_p - y_b)
+        sum += abs(x_b - x_e)
+        sum += abs(y_b - y_e)
+        print(sum)
+
