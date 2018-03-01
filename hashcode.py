b2414ecbb8a279d4fee52742649f75f8ba294454
faudil.puttilli <faudil.puttilli@epitech.eu>
2018-03-01 19:08:59 +0100

    classes moved to hashcode.py

---
+++ hashcode.py
@@ -0,0 +1,10 @@
+class Ride:
+    def __init__(self, begin, end, start, finish):
+        _begin = begin
+        _end = end
+        _start = start
+        _finish = finish
+
+class Vehicle:
+    def __init__(self):
+        _avail = True
