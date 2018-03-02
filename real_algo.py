4460c8a49fdf4aa46e3021c07bb71cac2b889a01
faudil.puttilli <faudil.puttilli@epitech.eu>
2018-03-02 10:45:09 +0100

    last commit with final verson of real algo which gave us 43 millions points

--- real_algo.py
+++ real_algo.py
@@ -1,46 +1,28 @@
 
 from hashcode import *
 
-def sort_rides(rides, car, step):
-    sort = rides[:]
-    tv = None
-    sorted = False
-    while (sorted == False):
-        sorted = True
-        for i in range(0, len(sort) - 1):
-            tv = car.total_ride_length(sort[i]) + sort[i]._start - step
-            if tv > car.total_ride_length(sort[i + 1]) + sort[i + 1]._start - step:
-                tmp = sort[i]
-                sort[i] = sort[i + 1]
-                sort[i + 1] = tmp
-                sorted = False
-    return sort
-
 def get_best_ride(rides, car, step):
     ld = 100000000000
     best = -1
     for i in range(0, len(rides)):
-        v = car.total_ride_length(rides[i]) + rides[i]._start + step
+        v = car.total_ride_length(rides[i]) + rides[i]._start - step
         if rides[i]._taken == False and ld >= v:
             best = i
             ld = v
     return best
 
-def fastest_car(vehicules):
-    return min(vehicules, key=lambda x: x._steps)._steps
-
 def real_solve(vehicles, rides, max_step):
     step = 0
     for remain in range(len(rides), 0, -1):
-        for vehicle_i in range(0, len(vehicles)):
-            if vehicles[vehicle_i]._steps == fastest_car(vehicles):
-                best_ride = get_best_ride(rides, vehicles[vehicle_i], vehicles[vehicle_i]._steps)
-                if best_ride < 0:
-                    continue
-                vehicles[vehicle_i]._rides.append(best_ride)
-                vehicles[vehicle_i]._steps += vehicles[vehicle_i].total_ride_length(rides[best_ride])
-                vehicles[vehicle_i].pos = rides[best_ride]._end
-                rides[best_ride]._taken = True
+        vehicle_i, ttt = min(enumerate(vehicles), key=lambda x: x[1]._steps)
+        best_ride = get_best_ride(rides, vehicles[vehicle_i], vehicles[vehicle_i]._steps)
+        if best_ride < 0:
+            remain += 1
+            continue
+        vehicles[vehicle_i]._rides.append(best_ride)
+        vehicles[vehicle_i]._steps += vehicles[vehicle_i].total_ride_length(rides[best_ride])
+        vehicles[vehicle_i]._pos = rides[best_ride]._end
+        rides[best_ride]._taken = True
     for i in range(0, len(vehicles)):
         print(len(vehicles[i]._rides), end=" ")
         for j in range(0, len(vehicles[i]._rides)):
